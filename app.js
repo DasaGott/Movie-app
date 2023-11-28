96a90f26a54957a41f64f97183f24a9c2028d5ba
DasaGott <142871686+DasaGott@users.noreply.github.com>
2023-11-28 17:35:02 +0100

    DOM manipulation from JS

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
-const api_url = `https://api.themoviedb.org/3/movie/popular?&api_key=487b92ab203942469fb2a6052ce57644&page=1`;
-const img_path = `https://image.tmdb.org/t/p/w1280`;
+const api_url = `https://api.themoviedb.org/3/movie/popular?&api_key=487b92ab203942469fb2a6052ce57644`;
+const img_path = `https://image.tmdb.org/t/p/w500`;
 const search_api = `https://api.themoviedb.org/3/search/movie?&api_key=487b92ab203942469fb2a6052ce57644&query="`;
 
 const searchForm =
   document.getElementById("form");
 const search = document.getElementById("search");
+const main = document.querySelector("main");
 
 get_movies(api_url);
 async function get_movies(url) {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const {
+      vote_average,
+      overview,
+      title,
+      poster_path,
+    } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `<img
+          src="${img_path + poster_path}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class=${getClassByRating(
+            vote_average
+          )}>${(
+      Math.round(vote_average * 100) / 100
+    ).toFixed(1)}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRating(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
 searchForm.addEventListener("submit", (event) => {
